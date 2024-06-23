5f70ca71c204bdb780d9ac333cd66aa3f4674b1f
ashwani <ashwani.bhadani.3289@gmail.com>
2024-06-23 22:09:17 +0530

    project development begins

---
+++ src/main/java/com/prospectManager/prospectManagerCore/ProspectManagerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.prospectManager.prospectManagerCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectManagerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectManagerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prospectManager/prospectManagerCore/ProspectManagerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prospectManager.prospectManagerCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProspectManagerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
